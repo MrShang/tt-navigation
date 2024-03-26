eed4c7d461bcb0fc52e7fd6d912d9e4e5af91b3b
chris.shang <chris.shang@sinobot.com.cn>
2024-03-26 15:22:12 +0800

    display map, plan a route

---
+++ src/TomTomMap.js
@@ -0,0 +1,127 @@
+import React, { useEffect, useRef, useState } from 'react';
+import tt from "@tomtom-international/web-sdk-maps";
+import { services } from '@tomtom-international/web-sdk-services';
+import SearchBox from '@tomtom-international/web-sdk-plugin-searchbox';
+import ZoomControls from '@tomtom-international/web-sdk-plugin-zoomcontrols';
+
+const TomTomMap = () => {
+    const mapContainerRef = useRef(null);
+    const [error, setError] = useState(null);
+    const API_KEY = 'slbuC9DI0UphoNVEMdii60WZB5GT6kut';
+    const options = {
+        searchOptions: {
+            key: API_KEY,
+            language: "en-GB",
+            limit: 5,
+        },
+        autocompleteOptions: {
+            key: API_KEY,
+            language: "en-GB",
+        },
+        noResultsMessage: 'No results found.'
+    };
+
+    const ttZoomControls = new ZoomControls({
+        className: '', // default = ''
+        animate: true // deafult = true
+    });
+
+    const getLocation = async () => {
+        return new Promise((resolve, reject) => {
+          navigator.geolocation.getCurrentPosition(
+            position => {
+              resolve(position);
+            },
+            error => {
+              reject(error);
+            }
+          );
+        });
+      };
+
+    let currentLocationIndex = 0;
+    const moveCurrentLocation = (map, routeCoordinates) => {
+        console.log('moving now....');
+        console.log({ currentLocationIndex });
+        console.log({ routeCoordinates });
+        console.log(`length: ${routeCoordinates.length}`);
+        if (currentLocationIndex < routeCoordinates.length) {
+            const currentLocation = routeCoordinates[currentLocationIndex];
+            map.setCenter(currentLocation);
+            currentLocationIndex++;
+            setTimeout(moveCurrentLocation(map, routeCoordinates), 100); // Update every 2 seconds (adjust as needed)
+        }
+    }
+
+    useEffect(() => {
+        const fetchLocation = async () => {
+          try {
+            const position = await getLocation();
+            const map = tt.map({
+                key: API_KEY,
+                container: mapContainerRef.current,
+                center: [position.coords.longitude, position.coords.latitude],
+                language: 'en-GB',
+                zoom: 8,
+                minZoom: 2,
+                maxZoom:12
+            });
+            const ttSearchBox = new SearchBox(services, options);
+            ttSearchBox.on('tomtom.searchbox.resultselected', data => {
+                console.log('selected data ....');
+                console.log({ data });
+                const locations = `${position.coords.longitude},${position.coords.latitude}:${data.data.result.position.lng},${data.data.result.position.lat}`;
+                console.log({ locations });
+                services.calculateRoute({
+                  key: API_KEY,
+                  locations: locations
+                }).then(function(routeData) {
+                    const geoData = routeData.toGeoJson();
+                    console.log({ geoData });
+                    map.addLayer({
+                        id: "route",
+                        type: "line",
+                        source: {
+                            type: "geojson",
+                            data: geoData
+                        },
+                        layout: {
+                            'line-join': 'round',
+                            'line-cap': 'round'
+                        },
+                        paint: {
+                            "line-color": "#FF0000",
+                            "line-width": 3
+                        }
+                    });
+                    console.log('starting navigation simulation...');
+//                    map.setPitch(50);
+//                    moveCurrentLocation(map, geoData.features[0].geometry.coordinates);
+//                    map.setPitch(0);
+                });
+            });
+
+            map.addControl(ttSearchBox, "top-left");
+            map.addControl(ttZoomControls, 'top-left');
+          } catch (error) {
+            console.error('Error fetching location:', error);
+          }
+        };
+        fetchLocation();
+        // 清理函数，在组件卸载时调用
+//        return () => {
+//            // 清理地图资源，如果需要的话
+//            if (map) {
+//                map.remove();
+//            }
+//        };
+    }, []); // 空依赖数组确保这个effect只运行一次
+
+    return (
+       <div>
+           <div ref={mapContainerRef} style={{ width: '100%', height: '400px' }}></div>
+       </div>
+   );
+};
+
+export default TomTomMap;
