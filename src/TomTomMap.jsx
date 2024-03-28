700876a1f550803c77454675765b1d1e7138c794
chris.shang <chris.shang@sinobot.com.cn>
2024-03-28 10:51:52 +0800

    refactor code

--- src/TomTomMap.jsx
+++ src/TomTomMap.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef } from 'react';
 import tt from "@tomtom-international/web-sdk-maps";
 import { services } from '@tomtom-international/web-sdk-services';
 import SearchBox from '@tomtom-international/web-sdk-plugin-searchbox';
@@ -10,141 +10,116 @@ import '@tomtom-international/web-sdk-plugin-zoomcontrols/dist/ZoomControls.css'
 const TomTomMap = () => {
     const mapContainerRef = useRef(null);
     const API_KEY = 'slbuC9DI0UphoNVEMdii60WZB5GT6kut';
-    const options = {
-        searchOptions: {
-            key: API_KEY,
-            language: "en-GB",
-            limit: 5,
-        },
-        noResultsMessage: 'No results found.'
+
+    const addMarkerToMap = (map, options, lgnlat) => {
+        new tt.Marker(options)
+            .setLngLat(lgnlat)
+            .addTo(map);
     };
 
-    const ttZoomControls = new ZoomControls({
-        className: '', // default = ''
-        animate: true // deafult = true
-    });
-
-    const getLocation = async () => {
-        return new Promise((resolve, reject) => {
-          navigator.geolocation.getCurrentPosition(
-            position => {
-              resolve(position);
+    const addRouteLayer = (map, geoData) => {
+        map.addLayer({
+            id: "route",
+            type: "line",
+            source: {
+                type: "geojson",
+                data: geoData
             },
-            error => {
-              reject(error);
+            layout: {
+                'line-join': 'round',
+                'line-cap': 'round'
+            },
+            paint: {
+                "line-color": "#FF0000",
+                "line-width": 3
             }
-          );
         });
-      };
+    };
 
-    let currentLocationIndex = 0;
-    const moveCurrentLocation = (map, routeCoordinates) => {
-        console.log('moving now....');
-        console.log({ currentLocationIndex });
-        console.log({ routeCoordinates });
-        console.log(`length: ${routeCoordinates.length}`);
-        if (currentLocationIndex < routeCoordinates.length) {
-            const currentLocation = routeCoordinates[currentLocationIndex];
-            map.setCenter(currentLocation);
-            new tt.Popup()
-                .setLngLat(currentLocation)
-                .setHTML("<p>Start</p>")
-                .addTo(map);
-            currentLocationIndex++;
-            setTimeout(moveCurrentLocation(map, routeCoordinates), 1); // Update every 2 seconds (adjust as needed)
-        }
-    }
+    const navigationSimulate = (map, geoData) => {
+        let currentStep = 0;
+        const stepInterval = 100; // Move to the next step every second
+        const routeGeometry = geoData.features[0].geometry.coordinates;
+        const popup = new tt.Popup()
+            .setLngLat([routeGeometry[0][0], routeGeometry[0][1]])
+            .setText("I'm here")
+            .addTo(map);
+        const simulateStep = () => {
+            if (currentStep < routeGeometry.length) {
+              const position = routeGeometry[currentStep];
+              map.panTo([position[0], position[1]]);
+              popup.setText(`I'm in spot ${currentStep}`);
+              popup.setLngLat([position[0], position[1]]);
+              currentStep++;
+              setTimeout(simulateStep, stepInterval);
+            }
+        };
+        simulateStep();
+    };
+
+    const searchSelected = (data, map, position) => {
+        console.log('selected data ....');
+        console.log({ data });
+        const locations = `${position.coords.longitude},${position.coords.latitude}:${data.data.result.position.lng},${data.data.result.position.lat}`;
+        console.log({ locations });
+        // add the destination marker
+        addMarkerToMap(map, { color: '#ff0000' }, [data.data.result.position.lng, data.data.result.position.lat]);
+        services.calculateRoute({
+            key: API_KEY,
+            locations: locations
+        }).then(routeData => {
+            const geoData = routeData.toGeoJson();
+            console.log({ geoData });
+            addRouteLayer(map, geoData);
+            console.log('starting navigation simulation...');
+            navigationSimulate(map, geoData);
+            // Enable 3D rendering for the map
+            map.setPitch(45);
+            map.setBearing(0);
+        });
+    };
+
+    const initSearchBox = (map, position) => {
+        const options = {
+            searchOptions: {
+                key: API_KEY,
+                language: "en-GB",
+                limit: 5,
+            },
+            noResultsMessage: 'No results found.'
+        };
+        const ttSearchBox = new SearchBox(services, options);
+        ttSearchBox.on('tomtom.searchbox.resultselected', data => {
+            searchSelected(data, map, position);
+        });
+        return ttSearchBox;
+    };
+
+    const initMap = (position) => {
+        const map = tt.map({
+            key: API_KEY,
+            container: mapContainerRef.current,
+            center: [position.coords.longitude, position.coords.latitude],
+            language: 'en-GB',
+            zoom: 8,
+            minZoom: 2,
+            maxZoom:12
+        });
+        // add a startMarker
+        addMarkerToMap(map, { color: '#0016ff' }, [position.coords.longitude, position.coords.latitude]);
+
+        // add a searchBox
+        const ttSearchBox = initSearchBox(map, position);
+
+        map.addControl(ttSearchBox, "top-left");
+        map.addControl(new ZoomControls(), 'top-left');
+    };
 
     useEffect(() => {
-        const fetchLocation = async () => {
-          try {
-            const position = await getLocation();
-            const map = tt.map({
-                key: API_KEY,
-                container: mapContainerRef.current,
-                center: [position.coords.longitude, position.coords.latitude],
-                language: 'en-GB',
-                zoom: 8,
-                minZoom: 2,
-                maxZoom:12
-            });
-            const marker = new tt.Marker({ color: '#0016ff' })
-                .setLngLat([position.coords.longitude, position.coords.latitude])
-                .addTo(map);
-
-            const ttSearchBox = new SearchBox(services, options);
-            ttSearchBox.on('tomtom.searchbox.resultselected', data => {
-                console.log('selected data ....');
-                console.log({ data });
-                const locations = `${position.coords.longitude},${position.coords.latitude}:${data.data.result.position.lng},${data.data.result.position.lat}`;
-                console.log({ locations });
-                new tt.Marker({ color: '#ff0000' })
-                    .setLngLat([data.data.result.position.lng, data.data.result.position.lat])
-                    .addTo(map);
-                services.calculateRoute({
-                  key: API_KEY,
-                  locations: locations
-                }).then(routeData => {
-                    const geoData = routeData.toGeoJson();
-                    console.log({ geoData });
-                    map.addLayer({
-                        id: "route",
-                        type: "line",
-                        source: {
-                            type: "geojson",
-                            data: geoData
-                        },
-                        layout: {
-                            'line-join': 'round',
-                            'line-cap': 'round'
-                        },
-                        paint: {
-                            "line-color": "#FF0000",
-                            "line-width": 3
-                        }
-                    });
-                    console.log('starting navigation simulation...');
-                    let currentStep = 0;
-                    const stepInterval = 100; // Move to the next step every second
-                    const routeGeometry = geoData.features[0].geometry.coordinates;
-                    const popup = new tt.Popup()
-                        .setLngLat([routeGeometry[0][0], routeGeometry[0][1]])
-                        .setText("I'm here")
-                        .addTo(map);
-                    const simulateStep = () => {
-                        if (currentStep < routeGeometry.length) {
-                          const position = routeGeometry[currentStep];
-                          map.panTo([position[0], position[1]]);
-                          popup.setText(`I'm in spot ${currentStep}`);
-                          popup.setLngLat([position[0], position[1]]);
-                          currentStep++;
-                          setTimeout(simulateStep, stepInterval);
-                        }
-                    };
-
-                    // Enable 3D rendering for the map
-                    map.setPitch(45);
-                    map.setBearing(0);
-
-                    simulateStep();
-                });
-            });
-
-            map.addControl(ttSearchBox, "top-left");
-            map.addControl(ttZoomControls, 'top-left');
-          } catch (error) {
-            console.error('Error fetching location:', error);
-          }
-        };
-        fetchLocation();
-        // 清理函数，在组件卸载时调用
-//        return () => {
-//            // 清理地图资源，如果需要的话
-//            if (map) {
-//                map.remove();
-//            }
-//        };
-    }, []); // 空依赖数组确保这个effect只运行一次
+        navigator.geolocation.getCurrentPosition(position => {
+            initMap(position);
+        });
+    });
 
     return (
        <div>
